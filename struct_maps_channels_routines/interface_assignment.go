7b1bb800fb7aa7786d199134da5776a7935cf852
Mohamed Abdul Kader <mmkader85@gmail.com>
2021-03-17 18:19:56 +0800

    Loop thru interfaces to call its method

--- struct_maps_channels_routines/interface_assignment.go
+++ struct_maps_channels_routines/interface_assignment.go
@@ -3,52 +3,52 @@ package main
 import "fmt"
 
 type square struct {
-	name string
+	name       string
 	sideLength float64
 }
 
 type triangle struct {
-	name string
-	base float64
+	name   string
+	base   float64
 	height float64
 }
 
 type shape interface {
-	getName() string
 	getArea() float64
+	printArea()
 }
 
 func main() {
 	t := triangle{
-		name: "Triangle",
+		name:   "Triangle",
 		base:   7,
 		height: 10,
 	}
-	printArea(t)
 
 	s := square{
-		name: "Square",
+		name:       "Square",
 		sideLength: 5,
 	}
-	printArea(s)
+
+	shapes := []shape{t, s}
+	for _, shape := range shapes {
+		shape.printArea()
+	}
 }
 
-func printArea(s shape) {
-	fmt.Printf("Area of %s is %0.2f\n", s.getName(), s.getArea())
-}
 
 func (t triangle) getArea() float64 {
 	return 0.5 * t.base * t.height
 }
 
-func (t triangle) getName() string {
-	return t.name
+func (t triangle) printArea() {
+	fmt.Printf("Area of %s is %0.2f\n", t.name, t.getArea())
 }
 
 func (s square) getArea() float64 {
 	return s.sideLength * s.sideLength
 }
 
-func (s square) getName() string {
-	return s.name
+func (s square) printArea() {
+	fmt.Printf("Area of %s is %0.2f\n", s.name, s.getArea())
 }
